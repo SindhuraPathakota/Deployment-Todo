762ec73d94f7a8fec92b7a8aa1527a3cd2fa169d
Jayakiran Reddy Konkala <jkonkala@isimtl.local>
2020-05-25 11:01:30 -0400

    Sql for todo tabel updated

--- flyway/sql/V1_3tables.sql
+++ flyway/sql/V1_3tables.sql
@@ -26,9 +26,11 @@ CREATE TABLE `todo` (
   `todo_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(256) NOT NULL,
   `list_id` int(11) NOT NULL,
+  `task_points` int(11) DEFAULT '0',
+  `lable_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`todo_id`),
   KEY `list_id` (`list_id`),
+  KEY `lable_ibfk_2_idx` (`lable_id`),
+  CONSTRAINT `lables_ibfk1` FOREIGN KEY (`lable_id`) REFERENCES `lables` (`lable_id`),
   CONSTRAINT `todo_ibfk_1` FOREIGN KEY (`list_id`) REFERENCES `todo_list` (`list_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
